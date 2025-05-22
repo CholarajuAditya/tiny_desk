6b5e7c532e50752e524164c9c0b2ba70a4230a34
Cholaraju_Aditya <bluhawk2004@gmail.com>
2025-05-22 16:33:28 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,20 +1,33 @@
-import express from "express"
-import "dotenv/config"
-const app = express()
-import cors from "cors"
+import express from "express";
+import "dotenv/config";
+const app = express();
+import cors from "cors";
 
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(
+    cors({
+        origin: "https://your-frontend.vercel.app",
+        credentials: true,
+    })
+);
 
 //routes
-import { reportRouter, loginRouter, registerRouter, profileRouter, customerRouter, vendorRouter, invoiceRouter, poRouter } from "./routes/index.js"
-import { auth } from "./utils/index.js"
+import {
+    reportRouter,
+    loginRouter,
+    registerRouter,
+    profileRouter,
+    customerRouter,
+    vendorRouter,
+    invoiceRouter,
+    poRouter,
+} from "./routes/index.js";
+import { auth } from "./utils/index.js";
 
-app.use("/login", loginRouter)
-app.use("/register", registerRouter)
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
 
-app.use(auth)
+app.use(auth);
 // import Customer from "./models/customer.model.js"
 // import data2 from "./data/customer.js"
 // import Vendor from "./models/vendor.model.js"
@@ -31,23 +44,20 @@ app.use(auth)
 // }
 // createUsers()
 
-
-app.use("/profile", profileRouter)
-app.use("/customer", customerRouter)
-app.use("/vendor", vendorRouter)
-app.use("/invoice", invoiceRouter)
-app.use("/po", poRouter)
-app.use("/report", reportRouter)
+app.use("/profile", profileRouter);
+app.use("/customer", customerRouter);
+app.use("/vendor", vendorRouter);
+app.use("/invoice", invoiceRouter);
+app.use("/po", poRouter);
+app.use("/report", reportRouter);
 //error handling
-import { errorHandler } from "./utils/index.js"
-app.use(errorHandler)
+import { errorHandler } from "./utils/index.js";
+app.use(errorHandler);
 
 //DB connect
-import {connectDB} from "./db/connectDB.js"
-const port = process.env.PORT
-app.listen(port, async() => {
-    await connectDB(process.env.MONGO_URI)
+import { connectDB } from "./db/connectDB.js";
+const port = process.env.PORT;
+app.listen(port, async () => {
+    await connectDB(process.env.MONGO_URI);
     console.log(`server listening on ${port}`);
-})
-
-
+});
