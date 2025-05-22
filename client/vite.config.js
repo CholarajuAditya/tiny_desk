dca244f2f0cf2dd2d64bb3a81edebaaec1ac77e3
Cholaraju_Aditya <bluhawk2004@gmail.com>
2025-05-22 16:35:25 +0530

    server_URI

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({ 
-  server: {
-    proxy: {
-      "/api": {  // ✅ Proxy only API requests
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Removes "/api" prefix
-      },
+export default defineConfig({
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://tiny-desk.onrender.com",
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+        plugins: [react(), tailwindcss()],
     },
-  },
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+});
